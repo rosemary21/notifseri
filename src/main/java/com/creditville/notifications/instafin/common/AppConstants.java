a46d011070556ec737cc65fe153f42491561ab21
unknown <chioma.chukelu@creditville.ng>
2021-12-20 11:41:40 +0100

    master

--- src/main/java/com/creditville/notifications/instafin/common/AppConstants.java
+++ src/main/java/com/creditville/notifications/instafin/common/AppConstants.java
@@ -5,6 +5,7 @@ public class AppConstants {
     public static class InstafinPaymentMethod{
         public static final String FROM_TO_ACCOUNT = "From/To Account";
         public static final String PAYSTACK_PAYMENT_METHOD = "PAYSTACK_PAYMENT_METHOD";
+        public static final String REMITA_PAYMENT_METHOD="REMITA_PAYMENT_METHOD";
     }
 
     public static final class InstafinBranch {
