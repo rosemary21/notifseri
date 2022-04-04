342a27b8e752102d5db213b8e628c1a5dd2efd0f
unknown <chioma.chukelu@creditville.ng>
2022-04-04 13:09:52 +0100

    sodchi

--- src/main/java/com/creditville/notifications/jobs/NotificationJobs.java
+++ src/main/java/com/creditville/notifications/jobs/NotificationJobs.java
@@ -57,7 +57,7 @@ public class NotificationJobs {
 //            log.info(cce.getMessage());
 //        }
 //    }
-
+//
 ////    @Scheduled(cron = "${app.schedule.dueRentalOne}")
 //     @Scheduled(cron = "${app.schedule.everyThirtySeconds}")
 //    public void sendAllNotification() {
