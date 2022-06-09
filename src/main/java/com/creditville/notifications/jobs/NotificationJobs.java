71592b807dbd189699035dffe70bd6211ed7b88e
creditvilleNg <chioma.chukelu@creditville.ng>
2022-06-09 10:20:33 +0100

    sofchi

--- src/main/java/com/creditville/notifications/jobs/NotificationJobs.java
+++ src/main/java/com/creditville/notifications/jobs/NotificationJobs.java
@@ -165,8 +165,8 @@ public class NotificationJobs {
 //        }
 //    }
 //////
-    @Scheduled(cron = "${app.schedule.recurringCharges}")
-//    @Scheduled(cron = "${app.schedule.everyThirtySeconds}")
+//    @Scheduled(cron = "${app.schedule.recurringCharges}")
+    @Scheduled(cron = "${app.schedule.everyThirtySeconds}")
     public void recurringChargesNotification() {
         if(recurringChargesEnabled)
             dispatcherService.performRecurringChargesOperation();
@@ -182,8 +182,8 @@ public class NotificationJobs {
 //    }
 //
 ////
-    @Scheduled(cron = "${app.schedule.partialDebit}")
-//    @Scheduled(cron = "${app.schedule.everyThirtySeconds}")
+//    @Scheduled(cron = "${app.schedule.partialDebit}")
+    @Scheduled(cron = "${app.schedule.everyThirtySeconds}")
     public void partialDebitOperation() {
         if(partialDebitEnabled)
             partialDebitService.performPartialDebitOp();
